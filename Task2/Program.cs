f2d2e5467036064cbc3b0c32f6d4a1d365b4394d
Oxygen <grandnorik@mail.ru>
2022-11-13 21:59:18 +0600

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,3 +4,40 @@
 [-4, -6, 89, 6] -> 0
 */
 
+int[] MakeArray(int n)
+{
+    int[] result = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        result[i] = rnd.Next(-100, 100);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int OddNumbers(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 != 0)
+        summ = summ + array[i];
+    }
+    return summ;
+}
+
+Console.Write("Введите длину массива: ");
+int number = int.Parse(Console.ReadLine());
+int[] arr = MakeArray(number);
+PrintArray(arr);
+int result = OddNumbers(arr);
+Console.WriteLine($"Сумма нечетных чисел в массиве: {result}");
